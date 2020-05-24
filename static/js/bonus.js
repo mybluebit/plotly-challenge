56b22d48790559732bf9030b0bd7ac1c53025cf9
Ahmad Mahroo <42006333+mybluebit@users.noreply.github.com>
2020-05-23 21:22:49 -0400

    Update bonus.js
    Uploading the right code of the bouns file

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,220 +1,113 @@
-d3.json("data/samples.json").then((data) => {
-    // To be sure that we get the data
-    console.log(data);
+//  ******************** Bonus ********************* //
 
-    // Assigning values to the variables
-    var metaData = data.metadata;
-    console.log("MetaData:", metaData);
+// Creating Gauge Chart
+function buildGauge(wfreq) {
+    // Enter the washing frequency between 0 and 180
+    // We have 9 steps so 180 / 9 = 20
+    var level = parseFloat(wfreq) * 20;
 
-    var names = data.names;
-    console.log("Names:", names);
+    // Trig to calc meter point
+    // To calculate the location of Trig from 0
+    var degrees = 180 - level;
 
-    var sampleData = data.samples;
-    console.log("SampleData:", sampleData);
-    
-    // Filling the Dropdown menu for Test Subject ID No.
-    var dropDown = d3.select("#selDataset");
-    names.forEach((name) =>{
-        var cell = dropDown.append("option");
-        cell.text(name);
-    });
+    // Length of Trig
+    var radius = 0.5;
 
-// ******************************************************//
+    // Angle of Trig for each WFREQ
+    var radians = (degrees * Math.PI) / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
 
-    // Function of Creating Bar Chart and Bubble Chart
-    function chartBuilder(sample){
+    // Path: may have to change to create a better triangle
+    var mainPath = "M -.0 -0.05 L .0 0.05 L ";
+    var pathX = String(x);
+    var space = " ";
+    var pathY = String(y);
+    var pathEnd = " Z";
+    var path = mainPath.concat(pathX, space, pathY, pathEnd);
 
-        // Creating Filter Sample from SampleData Based on the Selected ID
-        var filterSample = [];
-        filterSample = sampleData.filter(data => data.id === sample);
-        console.log(filterSample);
+    // Setting the circle at the center of Trig
+    var data = [
+    {
+        type: "scatter",
+        x: [0],
+        y: [0],
+        marker: { size: 20, color: "#f2096b" },
+        showlegend: false,
+        name: "Washing Frequency",
+        text: level,
+        hoverinfo: "text+name"
+    },
+    {
+        // Top Half of the circle should be divided to 9 section
+        values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
 
-        // Finding the values of sampleId, sampleValues & sampleLabel
-        var sampleId = [];
-        var sampleValues = [];
-        var sampleLabel = [];
+        // To show the chart horizontally
+        rotation: 90,
 
-        filterSample.forEach(info => {
-            Object.entries(info).forEach(([key,value]) =>{
+        // Inside text for each section, the last one which is for the bottom half of the chart should be null
+        text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+        textinfo: "text",
+        textposition: "inside",
 
-                switch(key){
-                    case ("otu_ids"):
-                        sampleId = value.map(y => y);
-                        console.log("sample ID:", sampleId);
-                        break;
-
-                    case("sample_values"):
-                        sampleValues = value.map( x => x);
-                        console.log("sample value:", sampleValues);
-                        break;
-
-                    case ("otu_labels"):
-                        sampleLabel = value.map(label => label);
-                        console.log("sample Labels:", sampleLabel);
-                        break;
-                };
-
-            });
-        });
-
-        // Finding the first Top 10 value of each array
-        var xAxis = sampleValues.slice(0,10);
-        var lable = sampleLabel.slice(0,10);
-
-        // Finding the first Top 10 values for OTU_IDS and adding "OTU" to them for the chart
-        var yAxis = [];
-        for (var i = 0; i < xAxis.length; i++){
-            yAxis.push("OTU " + sampleId[i]);
-        };
-
-        // Reversing the values for showing from topest to the least
-        xAxis.reverse();
-        yAxis.reverse();
-        lable.reverse();
-
-        // Checking the x and y values for the chart
-        console.log("xAxis:", xAxis);
-        console.log("yAxis:", yAxis);
-        console.log("Label:", lable);
+        // Everything should be set from right to left
         
-        // Creating Bar Chart:
-        var trace1 = {
-            x: xAxis,
-            y: yAxis,
-            text: lable,
-            name: "OTUs",
-            type: "bar",
-            orientation: "h",
-            marker: {
-                color: [
-                    "#E0B1CB",
-                    "#CBA0BC",
-                    "#B68FAD",
-                    "#A17E9D",
-                    "#8C6D8E",
-                    "#775D7F",
-                    "#624C70",
-                    "#4D3B60",
-                    "#382A51",
-                    "#231942"
+        marker: {
+        // Colour of each section of the chart
+        colors: [
+            "#85b788",
+            "#8bbf8f",
+            "#8dc386",
+            "#b7cf90",
+            "#d5e79a",
+            "#e5e9b0",
+            "#eae8ca",
+            "#f5f2e5",
+            "#f9f3ec",
+            "#ffffff"
+        ]
+        
+        },
+        // Labels of each section of the chart
+        labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+        hoverinfo: "label",
+        hole: 0.5,
+        type: "pie",
+        showlegend: false
+    }
+    ];
 
-                ]
-            }
-        };
-
-        var data = [trace1];
-
-        // Apply the group bar mode to the layout
-        var layout = {
-        title: `Top 10 OTUs Found in Test ID No.: ${sample}`,
-        xaxis: {title: 'Sample Values'},
-        yaxis: {title: 'OTUs'},
-        margin: {
-            l: 100,
-            r: 100,
-            t: 100,
-            b: 100
+    // Set of Trig shape and color and location in the chart
+    var layout = {
+    shapes: [
+        {
+        type: "path",
+        path: path,
+        fillcolor: "#f2096b",
+        line: {
+            color: "#f2096b"
         }
-        };
-
-        // Displaying the Bar Chart
-        Plotly.newPlot("bar", data, layout);
-
-// *****************************************************//
-
-        // Craeting Bubble Chart:
-        var trace2 = {
-            x: sampleId,
-            y: sampleValues,
-            text: sampleLabel,
-            mode: 'markers',
-            marker: {
-                color: sampleId,
-                size: sampleValues,
-                sizeref: 0.05,
-                sizemode: 'area'
-            }
-            };
-            
-        var data2 = [trace2];
-        
-        // Apply the mode to the layout
-        var layout2 = {
-            title: `OTUs Samples for Test ID No.: ${sample}`,
-            showlegend: false,
-            xaxis: {
-                title: "OTU IDs",
-                showgird: true
-            },
-        };
-        
-        // Displaying the Bubble Chart
-        Plotly.newPlot("bubble", data2, layout2);
+        }
+    ],
+    title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+    height: 500,
+    width: 500,
+    xaxis: {
+        zeroline: false,
+        showticklabels: false,
+        showgrid: false,
+        range: [-1,1]
+    },
+    yaxis: {
+        zeroline: false,
+        showticklabels: false,
+        showgrid: false,
+        range: [-1, 1]
+    }
     };
+// Displaying the Guage Chart
+    Plotly.newPlot("gauge", data, layout);
+};
 
-// *****************************************************//
+// **************** End of Bonus *********************** //
 
-    // Function for filling the Demographic Info Box
-    function tableBuilder(sample){
-
-        // Filtering the MetaData by Selected Test ID
-        var filterSample = [];
-        filterSample = metaData.filter(data => data.id == sample);
-        console.log("filtered MetaData:",filterSample);
-
-        // Assigning the variable based on the class of panel-body in html
-        var table = d3.select(".panel-body");
-        table.html("");  
-        
-        // Filling the Demographic Info Box:
-        filterSample.forEach(info => {
-
-            // Adding rows
-            var row = table.append("tr");
-
-            // Finding the key and value of the filter metaData and filling the rows
-            Object.entries(info).forEach(([key,value]) =>{
-
-                var row = table.append("tr");
-                var cell = row.append("td");
-
-                cell.text(`${key}: ${value}`);
-                
-                // Finding the value of wfreq for passing later to the buildGauge Function
-                
-                if (key === 'wfreq'){
-                    wfreqValue = value;
-                };
-                
-            });
-        });
-    };
-
-// *************************************************** //
-
-
-    // Defining the Changing Button:
-    d3.select("#selDataset").on("change", optionChanged);
-
-    // Function of the event for Change Button
-    function optionChanged(){
-        var testId = d3.select("#selDataset");
-        var selectedDropdown = testId.property("value");
-        console.log("Selected Drop Down:",selectedDropdown);
-       
-        // Creating Bar Chart and Bubble Chart
-        chartBuilder(selectedDropdown);
-
-        // Filling Demographic Info Box
-        tableBuilder(selectedDropdown);
-
-        // Creating Gauge Chart
-        buildGauge(wfreqValue)
-    };
-    
-    // Setting the default for the first time loading so that the page is not empty
-    chartBuilder('940');
-    tableBuilder('940');
-    buildGauge('2');
-
-});
